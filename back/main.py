2a7ee0a2db077c65b51a42fce3796a7aa03c70ac
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-03-24 00:15:20 +0700

    front

--- back/main.py
+++ back/main.py
@@ -10,15 +10,13 @@ import whisper
 
 app = FastAPI()
 
-# Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://127.0.0.1:8000", "http://localhost:8000"],  # Укажите адреса фронтенда
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Монтирование статических файлов
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -113,6 +111,8 @@ async def websocket_endpoint(websocket: WebSocket):
             if websocket.client_state == WebSocketState.CONNECTED:
                 await websocket.send_text(transcription)
                 log_transcription(transcription)
+        except Exception as e:
+            print(f"Ошибка при обработке аудио: {e}")
         finally:
             # Удаляем временный файл
             if os.path.exists(temp_filename):
